cb1022881f74d31094ce791d43131f7cf4ac4432
DESKTOP-BQNBV80\Lars Moesman <morty@gmail.com>
2018-02-01 16:50:24 +0100

    Created new solution

---
+++ Microcontrollers_Week1/Microcontrollers_Week1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Microcontrollers_Week1.c
+ *
+ * Created: 1-2-2018 16:49:35
+ * Author : Lars Moesman
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
