223b82c18eba8600c135641db99dea7181fb82f6
DESKTOP-BQNBV80\Lars Moesman <morty@gmail.com>
2018-02-02 11:44:14 +0100

    Made new main files

--- Microcontrollers_Week1/Microcontrollers_Week1/src/main.c
+++ Microcontrollers_Week1/Microcontrollers_Week1/src/main.c
@@ -1,24 +1,13 @@
-/* ---------------------------------------------------------------------------
-** This software is in the public domain, furnished "as is", without technical
-** support, and with no warranty, express or implied, as to its usefulness for
-** any purpose.
-**
-** knipper.c
-**
-** Beschrijving:	Toggle even en oneven leds PORTD  
-** Target:			AVR mcu
-** Build:			avr-gcc -std=c99 -Wall -O3 -mmcu=atmega128 -D F_CPU=8000000UL -c knipper.c
-**					avr-gcc -g -mmcu=atmega128 -o knipper.elf knipper.o
-**					avr-objcopy -O ihex knipper.elf knipper.hex 
-**					or type 'make'
-** Author: 			dkroeske@gmail.com
-** -------------------------------------------------------------------------*/
-
+/*
+ * main.c
+ *
+ * Created: 2-2-2018 11:39:10
+ *  Author: Lars Moesman & Rick Verstraten
+ */ 
 
 #include "main.h"
 #include <avr/io.h>
 #include <util/delay.h>
-
 
 /******************************************************************/
 void wait( int ms )
@@ -30,6 +19,7 @@ notes:			Busy wait, not very accurate. Make sure (external)
 				clock value is set. This is used by _delay_ms inside
 				util/delay.h
 Version :    	DMK, Initial code
+Author	:		dkroeske@gmail.com
 *******************************************************************/
 {
 	for (int i=0; i<ms; i++)
@@ -40,23 +30,20 @@ Version :    	DMK, Initial code
 
 /******************************************************************/
 int main( void )
-/* 
+/*
 short:			main() loop, entry point of executable
-inputs:			
-outputs:	
+inputs:
+outputs:
 notes:			Looping forever, flipping bits on PORTD
 Version :    	DMK, Initial code
 *******************************************************************/
 {
 	
-	DDRD = 0b11111111;			// All pins PORTD are set to output 
+
 	
 	while (1)
 	{
-		PORTD = 0xAA;			// Write 10101010b PORTD
-		wait( 250 );				
-		PORTD = 0x55;			// Write 01010101b PORTD
-		wait( 250 );				
+		
 	}
 
 	return 1;
--- Microcontrollers_Week1/Microcontrollers_Week1/src/mainExample.h
+++ Microcontrollers_Week1/Microcontrollers_Week1/src/mainExample.h
@@ -1,16 +1,17 @@
 /*
  * main.h
  *
- * Created: 2-2-2018 11:39:41
- *  Author: Lars Moesman
+ * Created: 1-2-2018 16:27:37
+ *  Author: Rick Verstraten & Lars Moesman
  */ 
 
 
 #ifndef MAIN_H_
 #define MAIN_H_
 #define F_CPU 8000000
-
 void wait(int);
+
+
 
 
 #endif /* MAIN_H_ */
